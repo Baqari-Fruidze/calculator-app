c38e3a5ed68141d943d218fa6da52deca866f046
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-03-24 23:57:57 +0400

    slider fixed,some script done

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const slider = document.querySelector(".slider");
-const h3 = document.querySelector("h3");
-const body = document.body;
-let sliderValue;
-slider.addEventListener("change", () => {
-  sliderValue = Number(slider.value);
-  if (sliderValue === 3) {
-    h3.style.display = "none";
-  } else {
-    h3.style.display = "block";
-  }
-});
+// const slider = document.querySelector(".slider");
+// const h3 = document.querySelector("h3");
+// const body = document.body;
+// let sliderValue;
+// slider.addEventListener("change", () => {
+//   sliderValue = Number(slider.value);
+//   if (sliderValue === 3) {
+//     h3.style.display = "none";
+//   } else {
+//     h3.style.display = "block";
+//   }
+// });
+
+const nums = Array.from(document.querySelectorAll(".num"));
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const divide = document.querySelector(".divide");
+const multiply = document.querySelector(".mult");
+const equal = document.querySelector(".equal");
+const dot = document.querySelector(".dot");
+const reset = document.querySelector("reset");
+const Delete = document.querySelector(".delete");
