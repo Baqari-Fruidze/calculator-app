9dea45dcb8b2c4f06588fbea4462cfcc691112cf
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-03-25 23:29:33 +0400

    almost done

--- script.js
+++ script.js
@@ -23,18 +23,65 @@ const equal = document.querySelector(".equal");
 const dot = document.querySelector(".dot");
 const reset = document.querySelector("reset");
 const Delete = document.querySelector(".delete");
-const display = document.querySelector(".display");
 const container = document.querySelector(".main-container ");
+const display = document.querySelector(".display");
+const secondaryDisplay = document.querySelector(".secondary-dis");
 
 let firstHalf;
 let secondHalf;
 
 container.addEventListener("click", (event) => {
   if (event.target.textContent.length > 5) return;
+  if (event.target.textContent === "Del") {
+    display.textContent = display.textContent.slice(
+      0,
+      display.textContent.length - 1
+    );
+    return;
+  }
   display.textContent = display.textContent + event.target.textContent;
   if (event.target.textContent === "+") {
     firstHalf = display.textContent.slice(0, display.textContent.length - 1);
-    // display.textContent = "";
-    // display.textContent = display.textContent + event.target.textContent;
+    secondaryDisplay.textContent = display.textContent;
+    display.textContent = "";
+  }
+  if (event.target.textContent === "=") {
+    secondHalf = display.textContent.slice(0, display.textContent.length - 1);
+    if (secondaryDisplay.textContent.includes("+")) {
+      display.textContent = Number(firstHalf) + Number(secondHalf);
+    }
+    if (secondaryDisplay.textContent.includes("-")) {
+      display.textContent = Number(firstHalf) - Number(secondHalf);
+    }
+    if (secondaryDisplay.textContent.includes("*")) {
+      display.textContent = Number(firstHalf) * Number(secondHalf);
+    }
+    if (secondaryDisplay.textContent.includes("/")) {
+      display.textContent = Number(firstHalf) / Number(secondHalf);
+    }
+  }
+  if (event.target.textContent === "-") {
+    firstHalf = display.textContent.slice(0, display.textContent.length - 1);
+    secondaryDisplay.textContent = display.textContent;
+    display.textContent = "";
+  }
+  if (event.target.textContent === "*") {
+    firstHalf = display.textContent.slice(0, display.textContent.length - 1);
+    secondaryDisplay.textContent = display.textContent;
+    display.textContent = "";
+  }
+  if (event.target.textContent === "/") {
+    firstHalf = display.textContent.slice(0, display.textContent.length - 1);
+    secondaryDisplay.textContent = display.textContent;
+    display.textContent = "";
+  }
+  if (event.target.textContent === "reset") {
+    firstHalf = 0;
+    secondHalf = 0;
+    secondaryDisplay.textContent = "";
+    display.textContent = "";
+  }
+  if (display.textContent.length > 13) {
+    alert("are you sure? it is too much");
   }
 });
