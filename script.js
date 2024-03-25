5c0d4c19491800227cd3378c53ab26ef88311b29
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-03-25 20:15:50 +0400

    start different themes

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// const slider = document.querySelector(".slider");
-// const h3 = document.querySelector("h3");
-// const body = document.body;
-// let sliderValue;
-// slider.addEventListener("change", () => {
-//   sliderValue = Number(slider.value);
-//   if (sliderValue === 3) {
-//     h3.style.display = "none";
-//   } else {
-//     h3.style.display = "block";
-//   }
-// });
+const slider = document.querySelector(".slider");
+const body = document.body;
+slider.addEventListener("change", () => {
+  sliderValue = Number(slider.value);
+  if (sliderValue === 3) {
+    document.body.classList.add("dark");
+  }
+  if (sliderValue === 2) {
+    document.body.classList.remove("dark");
+  }
+});
 
 const nums = Array.from(document.querySelectorAll(".num"));
 const plus = document.querySelector(".plus");
@@ -21,3 +20,17 @@ const dot = document.querySelector(".dot");
 const reset = document.querySelector("reset");
 const Delete = document.querySelector(".delete");
 const display = document.querySelector(".display");
+const container = document.querySelector(".main-container ");
+
+let firstHalf;
+let secondHalf;
+
+container.addEventListener("click", (event) => {
+  if (event.target.textContent.length > 5) return;
+  display.textContent = display.textContent + event.target.textContent;
+  if (event.target.textContent === "+") {
+    firstHalf = display.textContent.slice(0, display.textContent.length - 1);
+    // display.textContent = "";
+    // display.textContent = display.textContent + event.target.textContent;
+  }
+});
